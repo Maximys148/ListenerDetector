a22d403e3308253feb84d3a12bcdc4ed6eb97331
Maximys <maxp.250705@gmail.com>
2025-07-01 09:44:51 +0300

    Создание проекта

---
+++ src/main/java/com/stupor/listenerdetector/ListenerDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.stupor.listenerdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListenerDetectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stupor/listenerdetector/ListenerDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stupor.listenerdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenerDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
